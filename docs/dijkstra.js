a9e7a9a90595a917ab708cdc83a32b3d3725f9e1
millibar <millibarjp@gmail.com>
2022-08-13 16:46:11 +0900

    ダイクストラとテストを追加

---
+++ docs/dijkstra.js
@@ -0,0 +1,185 @@
+
+/**
+ * 始点となる駅名と路線名を与えると、次の駅名を返す。
+ * @param {string} stationName 始点となる駅名
+ * @param {string} lineName 始点となる駅の路線名
+ * @param {Array.<Edge>} edges 駅のつながりの配列
+ * @returns {string} 次の駅名
+ */
+const getNextStationName = (stationName, lineName, edges) => {
+    return edges.filter(e => e.name === stationName && e.line === lineName)[0].next;
+}
+
+/**
+ * 駅名を与えると、その駅の路線名の配列を返す。
+ * @param {string} stationName 駅名 
+ * @param {Array.<Edge>} edges 駅のつながりの配列
+ * @returns {Array.<string>} 路線名
+ */
+const getLineNames = (stationName, edges) => {
+    return edges.filter(e => e.name == stationName).map(e => e.line);
+}
+
+/**
+ * 昇順に並んだ数値の配列から nより大きい最小値を求める。
+ * @param {Array.<number>} array 
+ * @param {number} n 
+ * @returns {number} nより大きい最小値
+ */
+const minValueGreaterThan = (array, n) => {
+    return array.filter(e => e > n)[0]; // 2分探索すればもっと早いかも nより大きい値がない場合は？
+}
+
+/**
+ * 指定した駅名、路線名の時刻表から、tより大きい最小の時刻の値を取得する
+ * @param {string} stationName 駅名
+ * @param {string} lineName 路線名
+ * @param {number} t 時刻
+ * @param {Array.<Edge>} edges 時刻表
+ * @returns {number} その駅の時刻表のうち、tより大きい最小の値
+ */
+const calcShortestTime = (stationName, lineName, t, edges) => {
+    const target = edges.filter(e => e.name === stationName && e.line === lineName)[0].time;
+    return minValueGreaterThan(target, t);
+}
+
+/**
+ * 最初の駅名と出発時刻、時刻表からNodeのリストの初期状態を作る
+ * @param {string} S0 最初の駅の名前
+ * @param {number} T0 出発時刻
+ * @param {Array.<Edge>} edges 時刻表
+ * @returns {Array.<Node>} Nodeのリストの初期状態 
+ */
+const initNodes = (S0, T0, edges) => {
+    const lineNames = getLineNames(S0, edges);
+
+    return edges.map(e => {
+        const name = e.name;
+        const line = e.line;
+        
+        const isTarget = (S0 === name && lineNames.includes(line));
+        
+        const shortestPath = isTarget ? [name] : [];
+        const shortestTime = isTarget ? calcShortestTime(S0, line, T0, edges) : Infinity;
+        return { name, line, shortestPath, shortestTime };
+    });
+}
+
+/**
+ * Nodeのリストを与えると、「shortestTimeが最小のNode」pと「それ以外のNodeのリスト」Vの組（配列）を返す
+ * @param {Array.<Node>} nodes 
+ * @returns {Array} [p, V]
+ */
+const separeteShortestTimeNode = (nodes) => {
+    if (!nodes.length) {
+        return [];
+    }
+    const [first, ...rest] = nodes;
+    const [p, V] = separeteShortestTimeNode(rest);
+
+    if (!p) {
+        return [first, []];
+    }
+
+    return first.shortestTime <= p.shortestTime ? [first, [...rest] ] : [p, [first, ...V] ];
+}
+
+/**
+ * 時刻表において、駅p→駅qと連続していればtrue。同じ駅名（すなわち乗り換え）の場合もtrue
+ * @param {Node} p 駅p
+ * @param {Node} q 駅q
+ * @param {Array.<Edge>} edges 時刻表
+ * @returns {boolean}
+ */
+const isConnected = (p, q, edges) => {
+    if (p.name === q.name) {
+        return true;
+    }
+    const nextStationName = getNextStationName(p.name, p.line, edges);
+    return nextStationName === q.name && p.line === q.line;
+}
+
+/**
+ * 直前に確定したNode p、未確定のNode qのリストV、Edgeのリストを与えると、必要な更新処理を行った後の未確定のNodeのリストを返す
+ * @param {Node} p 直前に確定したNode
+ * @param {Array.<Node>} V 未確定のNode qのリスト
+ * @param {Array.<Edge>} edges 
+ * @returns {Array.<Node>} 更新処理後の未確定のNodeのリストV'
+ * 
+ */
+const update = (p, V, edges) => {
+    return V.map(q => {
+        /**
+         * 直前に確定したNode pと未確定のNode qを与えると、pとqが直接つながっているかどうかを調べ、
+         * つながっているかつpの確定時刻の直後のqの時刻表の時刻が、現在のqのshortestTimeより小さいなら、
+         * shortestPathとshortestTimeを更新したqを、そうでないなら、qをそのまま返す
+         * @param {Node} p 直前に確定したNode
+         * @param {Node} q 未確定のNode
+         * @returns {Node} 更新後のq
+         */
+        if (isConnected(p, q, edges)) {
+            // ToDo qが終点の場合、qのtime = []（空の配列）
+
+            const Tp = p.shortestTime;
+            // pとqの路線が異なる場合、乗り換え時間として Tx = 4min を加える
+            const Tx = p.line === q.line ? 0 : 4;
+            const Tq = calcShortestTime(q.name, q.line, Tp + Tx, edges);
+            if (Tq < q.shortestTime) {
+                q.shortestTime = Tq;
+                // 乗り換えがある場合、同じ駅名になる
+                q.shortestPath = q.name === p.shortestPath[0] ? [...p.shortestPath] : [q.name, ...p.shortestPath];
+            }
+        };
+        return q;
+    });
+}
+
+/**
+ * 未確定のNodeのリストVとEdgeのリストを与えると、
+ * ダイクストラ法により各NodeのshortestPathとshortestTimeを更新したNodeのリストを返す
+ * @param {Array.<Node>} V 未確定のNodeのリスト
+ * @param {Array.<Edge>} edges 
+ * @returns {Array.<Node>} 確定したNodeのリスト（shortestTime昇順）
+ */
+const dijkstraMain = (V, edges) => {
+    if (!V.length) {
+        return [];
+    }
+    const [p, V0] = separeteShortestTimeNode(V);
+    const V1 = update(p, V0, edges);
+    return [p, ...dijkstraMain(V1, edges)];
+}
+
+/**
+ * 始点となる駅名、終点となる駅名、出発時刻を与えると、ダイクストラ法で求めた終点のNodeを返す
+ * @param {string} S0 出発駅のname
+ * @param {number} T0 出発時刻（秒）
+ * @param {string} G 到着駅のname
+ * @param {Array.<Edge>} edges 時刻表
+ * @returns {Node} 到着駅の情報
+ */
+ const dijkstra = (S0, T0, G, edges) => {
+    const V = initNodes(S0, T0, edges);
+    const U = dijkstraMain(V, edges);
+    return U.filter(node => node.name === G)[0];
+}
+
+export {
+    getNextStationName,
+    getLineNames,
+    calcShortestTime,
+    initNodes,
+    separeteShortestTimeNode,
+    isConnected,
+    update,
+    dijkstraMain,
+    dijkstra
+};
+
+
+
+
+
+
+
+
---
+++ docs/dijkstra.test.js
@@ -0,0 +1,237 @@
+import { schedule } from './schedule.js';
+import { getNextStationName, getLineNames, calcShortestTime, initNodes, separeteShortestTimeNode, isConnected, update, dijkstraMain, dijkstra } from './dijkstra.js';
+
+
+test.each([
+    ['名古屋', '桜通線（徳重行）', schedule, '国際センター'],
+    ['名古屋', '桜通線（中村区役所行）', schedule, '中村区役所'],
+    ['名古屋', '東山線（藤が丘行）', schedule, '伏見']
+])('%#. getNextStationName %s %s', (stationName, lineName, schedule, expected) => {
+    expect(getNextStationName(stationName, lineName, schedule)).toBe(expected);
+});
+
+test.each([
+    ['名古屋', schedule, ['東山線（高畑行）', '東山線（藤が丘行）', '桜通線（中村区役所行）', '桜通線（徳重行）'] ],
+    ['栄', schedule, ['東山線（高畑行）', '東山線（藤が丘行）', '名城線（左回り）', '名城線（右回り）']],
+    ['鶴舞', schedule, ['鶴舞線（上小田井行）', '鶴舞線（赤池行）']]
+])('%#. getLineNames %s', (stationName, schedule, expected) => {
+    expect(getLineNames(stationName, schedule)).toStrictEqual(expected);
+});
+
+
+
+
+const edgesForTest = [
+    {
+        name: '池下',
+        next: '今池',
+        line: '東山線（高畑行）',
+        time: [3,8,13,18,23,28,33,38,43,48,53,58]
+    },{
+        name: '今池',
+        next: '千種',
+        line: '東山線（高畑行）',
+        time: [4,9,14,19,24,29,34,39,44,49,54,59]
+    },{
+        name: '今池',
+        next: '池下',
+        line: '東山線（藤が丘行）',
+        time: [2,7,12,17,22,27,32,37,42,47,52,57]
+    },{
+        name: '池下',
+        next: null,
+        line: '東山線（藤が丘行）',
+        time: []
+    },{
+        name: '桜山',
+        next: '御器所',
+        line: '桜通線（中村区役所行）',
+        time: [5,15,25,35,45,55]
+    },{
+        name: '御器所',
+        next: '吹上',
+        line: '桜通線（中村区役所行）',
+        time: [6,16,26,36,46,56]
+    },{
+        name: '御器所',
+        next: '桜山',
+        line: '桜通線（徳重行）',
+        time: [7,17,27,37,47,57]
+    },{
+        name: '桜山',
+        next: null,
+        line: '桜通線（徳重行）',
+        time: []
+    }
+];
+
+
+test.each([
+    ['今池', '東山線（高畑行）', 24, edgesForTest, 29 ],
+    ['今池', '東山線（高畑行）', 23, edgesForTest, 24 ],
+    ['今池', '東山線（高畑行）', 0, edgesForTest, 4 ],
+    ['今池', '東山線（高畑行）', 55, edgesForTest, 59 ],
+    ['桜山', '桜通線（中村区役所行）', 16, edgesForTest, 25]
+])('%#. calcShortestTime %s %s %i', (stationName, lineName, t, edges, expected) => {
+    expect(calcShortestTime(stationName, lineName, t, edges)).toBe(expected);
+});
+
+
+test('initNodes 今池, 3', () => {
+    const actual = initNodes('今池', 3, edgesForTest);
+    const expected = [
+        { name: '池下', line: '東山線（高畑行）', shortestPath: [], shortestTime: Infinity },
+        { name: '今池', line: '東山線（高畑行）', shortestPath: ['今池'], shortestTime: 4 },
+        { name: '今池', line: '東山線（藤が丘行）', shortestPath: ['今池'], shortestTime: 7 },
+        { name: '池下', line: '東山線（藤が丘行）', shortestPath: [], shortestTime: Infinity },
+        { name: '桜山', line: '桜通線（中村区役所行）', shortestPath: [], shortestTime: Infinity },
+        { name: '御器所', line: '桜通線（中村区役所行）', shortestPath: [], shortestTime: Infinity },
+        { name: '御器所', line: '桜通線（徳重行）', shortestPath: [], shortestTime: Infinity },
+        { name: '桜山', line: '桜通線（徳重行）', shortestPath: [], shortestTime: Infinity }
+    ];
+    expect(actual).toStrictEqual(expected);
+});
+
+test('0. separeteShortestTimeNode', () => {
+    const actual = separeteShortestTimeNode([
+        { name: '池下', line: '東山線（高畑行）', shortestPath: [], shortestTime: Infinity },
+        { name: '今池', line: '東山線（高畑行）', shortestPath: ['今池'], shortestTime: 4 },
+        { name: '今池', line: '東山線（藤が丘行）', shortestPath: ['今池'], shortestTime: 7 },
+        { name: '池下', line: '東山線（藤が丘行）', shortestPath: [], shortestTime: Infinity }
+    ]);
+    const expected = [
+        { name: '今池', line: '東山線（高畑行）', shortestPath: ['今池'], shortestTime: 4 },
+        [
+            { name: '池下', line: '東山線（高畑行）', shortestPath: [], shortestTime: Infinity },
+            { name: '今池', line: '東山線（藤が丘行）', shortestPath: ['今池'], shortestTime: 7 },
+            { name: '池下', line: '東山線（藤が丘行）', shortestPath: [], shortestTime: Infinity }
+        ]
+    ]
+    expect(actual).toStrictEqual(expected);
+});
+
+test('1. separeteShortestTimeNode', () => {
+    const actual = separeteShortestTimeNode([
+        { name: '今池', line: '東山線（藤が丘行）', shortestPath: ['今池'], shortestTime: 7 },
+        { name: '池下', line: '東山線（藤が丘行）', shortestPath: [], shortestTime: Infinity },
+        { name: '桜山', line: '桜通線（中村区役所行）', shortestPath: [], shortestTime: Infinity },
+        { name: '御器所', line: '桜通線（中村区役所行）', shortestPath: [], shortestTime: Infinity }
+    ]);
+    const expected = [
+        { name: '今池', line: '東山線（藤が丘行）', shortestPath: ['今池'], shortestTime: 7 },
+        [
+            { name: '池下', line: '東山線（藤が丘行）', shortestPath: [], shortestTime: Infinity },
+            { name: '桜山', line: '桜通線（中村区役所行）', shortestPath: [], shortestTime: Infinity },
+            { name: '御器所', line: '桜通線（中村区役所行）', shortestPath: [], shortestTime: Infinity }
+        ]
+    ]
+    expect(actual).toStrictEqual(expected);
+});
+
+test('2. separeteShortestTimeNode', () => {
+    const actual = separeteShortestTimeNode([
+        { name: '池下', line: '東山線（高畑行）', shortestPath: [], shortestTime: Infinity },
+        { name: '今池', line: '東山線（高畑行）', shortestPath: ['今池'], shortestTime: 4 }
+    ]);
+    const expected = [
+        { name: '今池', line: '東山線（高畑行）', shortestPath: ['今池'], shortestTime: 4 },
+        [
+            { name: '池下', line: '東山線（高畑行）', shortestPath: [], shortestTime: Infinity }
+        ]
+    ]
+    expect(actual).toStrictEqual(expected);
+});
+
+test.each([
+    [{ name: '名古屋', line: '東山線（高畑行）', shortestPath: [], shortestTime: Infinity },
+     { name: '名古屋', line: '桜通線（徳重行）', shortestPath: [], shortestTime: Infinity }, schedule, true],
+
+    [{ name: '伏見', line: '鶴舞線（上小田井行）', shortestPath: ['伏見'], shortestTime: 1 },
+     { name: '丸の内', line: '鶴舞線（上小田井行）', shortestPath: [], shortestTime: Infinity }, schedule, true],
+
+    [{ name: '伏見', line: '鶴舞線（上小田井行）', shortestPath: ['伏見'], shortestTime: 1 },
+     { name: '丸の内', line: '桜通線（中村区役所行）', shortestPath: [], shortestTime: Infinity }, schedule, false],
+
+    [{ name: '伏見', line: '鶴舞線（上小田井行）', shortestPath: ['伏見'], shortestTime: 1 },
+     { name: '大須観音', line: '鶴舞線（上小田井行）', shortestPath: [], shortestTime: Infinity }, schedule, false],
+
+     [{ name: '大須観音', line: '鶴舞線（上小田井行）', shortestPath: ['伏見'], shortestTime: 1 },
+      { name: '伏見', line: '鶴舞線（上小田井行）', shortestPath: [], shortestTime: Infinity }, schedule, true],
+
+    [{ name: '伏見', line: '鶴舞線（上小田井行）', shortestPath: ['伏見'], shortestTime: 1 },
+     { name: '浅間町', line: '鶴舞線（上小田井行）', shortestPath: [], shortestTime: Infinity }, schedule, false]
+])('%#. isConnected', (p, q, edges, expected) => {
+    expect(isConnected(p, q, edges)).toBe(expected);
+});
+
+test('0. update 伏見 鶴舞線（上小田井行）', () => {
+    const p = { name: '伏見', line: '鶴舞線（上小田井行）', shortestPath: ['伏見'], shortestTime: 1 };
+    const V = [
+        { name: '伏見', line: '東山線（高畑行）', shortestPath: ['伏見'], shortestTime: 2 },
+        { name: '名古屋', line: '東山線（高畑行）', shortestPath: [], shortestTime: Infinity },
+        { name: '名古屋', line: '桜通線（徳重行）', shortestPath: [], shortestTime: Infinity },
+        { name: '国際センター', line: '桜通線（徳重行）', shortestPath: [], shortestTime: Infinity },
+        { name: '丸の内', line: '鶴舞線（上小田井行）', shortestPath: [], shortestTime: Infinity },
+        { name: '丸の内', line: '桜通線（中村区役所行）', shortestPath: [], shortestTime: Infinity }
+    ];
+    const expected = [
+        { name: '伏見', line: '東山線（高畑行）', shortestPath: ['伏見'], shortestTime: 2 },
+        { name: '名古屋', line: '東山線（高畑行）', shortestPath: [], shortestTime: Infinity },
+        { name: '名古屋', line: '桜通線（徳重行）', shortestPath: [], shortestTime: Infinity },
+        { name: '国際センター', line: '桜通線（徳重行）', shortestPath: [], shortestTime: Infinity },
+        { name: '丸の内', line: '鶴舞線（上小田井行）', shortestPath: ['丸の内','伏見'], shortestTime: 2 },
+        { name: '丸の内', line: '桜通線（中村区役所行）', shortestPath: [], shortestTime: Infinity }
+    ];
+    const actual = update(p, V, schedule);
+    expect(actual).toStrictEqual(expected);
+});
+
+
+test('dijkstraMain 伏見 → 国際センター', () => {
+    const V = [
+        { name: '伏見', line: '東山線（高畑行）', shortestPath: ['伏見'], shortestTime: 2 },
+        { name: '名古屋', line: '東山線（高畑行）', shortestPath: [], shortestTime: Infinity },
+        { name: '丸の内', line: '桜通線（中村区役所行）', shortestPath: [], shortestTime: Infinity },
+        { name: '国際センター', line: '桜通線（中村区役所行）', shortestPath: [], shortestTime: Infinity },
+        { name: '名古屋', line: '桜通線（徳重行）', shortestPath: [], shortestTime: Infinity },
+        { name: '国際センター', line: '桜通線（徳重行）', shortestPath: [], shortestTime: Infinity },
+        { name: '伏見', line: '鶴舞線（上小田井行）', shortestPath: ['伏見'], shortestTime: 1 },
+        { name: '丸の内', line: '鶴舞線（上小田井行）', shortestPath: [], shortestTime: Infinity }
+    ];
+    const actual = dijkstraMain(V, schedule);
+    const expected = [
+        { name: '伏見', line: '鶴舞線（上小田井行）', shortestPath: ['伏見'], shortestTime: 1 },
+        { name: '伏見', line: '東山線（高畑行）', shortestPath: ['伏見'], shortestTime: 2 },
+        { name: '丸の内', line: '鶴舞線（上小田井行）', shortestPath: ['丸の内','伏見'], shortestTime: 2 },
+        { name: '名古屋', line: '東山線（高畑行）', shortestPath: ['名古屋','伏見'], shortestTime: 5 },
+        { name: '丸の内', line: '桜通線（中村区役所行）', shortestPath: ['丸の内','伏見'], shortestTime: 8 },
+        { name: '国際センター', line: '桜通線（中村区役所行）', shortestPath: ['国際センター','丸の内','伏見'], shortestTime: 10 },
+        { name: '名古屋', line: '桜通線（徳重行）', shortestPath: ['名古屋','伏見'], shortestTime: 12 },
+        { name: '国際センター', line: '桜通線（徳重行）', shortestPath: ['国際センター','名古屋','伏見'], shortestTime: 13 }
+    ];
+    expect(actual).toStrictEqual(expected);
+});
+
+test('0. dijkstra 伏見 → 国際センター 0', () => {
+    expect(dijkstra('伏見', 0, '国際センター', schedule)).toStrictEqual(
+        { name: '国際センター', line: '桜通線（中村区役所行）', shortestPath: ['国際センター','丸の内','伏見'], shortestTime: 10 }
+    );
+});
+
+test('1. dijkstra 鶴舞 → 千種 0', () => {
+    expect(dijkstra('鶴舞', 0, '千種', schedule)).toStrictEqual(
+        { name: '千種', line: '東山線（藤が丘行）', shortestPath: ['千種','新栄町','栄','伏見','大須観音','上前津','鶴舞'], shortestTime: 25 }
+    );
+});
+
+test('2. dijkstra 荒畑 → 今池 10', () => {
+    expect(dijkstra('荒畑', 10, '今池', schedule)).toStrictEqual(
+        { name: '今池', line: '桜通線（中村区役所行）', shortestPath: ['今池','吹上','御器所','荒畑'], shortestTime: 30 }
+    );
+});
+
+test('3. dijkstra 荒畑 → 今池 11', () => {
+    expect(dijkstra('荒畑', 11, '今池', schedule)).toStrictEqual(
+        { name: '今池', line: '東山線（藤が丘行）', shortestPath: ['今池','千種','新栄町','栄','伏見','大須観音','上前津','鶴舞','荒畑'], shortestTime: 37 }
+    );
+});
