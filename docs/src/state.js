35411a0660dde54d7f985fbab519ac5d3ef30ee3
millibar <millibarjp@gmail.com>
2022-10-10 12:43:56 +0900

    hundleDijkstraからViewの更新処理をhundleTimetableに移動

--- docs/src/state.js
+++ docs/src/state.js
@@ -51,19 +51,6 @@ const setDijkstraResult = (state, result) => {
     };
 }
 
-/**
- * ダイクストラ法による最短経路問題で、到着駅と到着時刻をもった状態を返す
- * @param {State} state 状態
- * @param {Array.<string, numver>} end 到着駅名と到着時刻
- * @returns {State} 更新後の状態
- */
- const setDijkstraEnd = (state, end) => {
-    return {
-        ...state,
-        dijkstraEnd: end
-    };
-}
-
 export {
-    setDayType, setDijkstraResult, setDijkstraStart, setDijkstraEnd, setScheduleArray, setTrainGenerator
+    setDayType, setDijkstraResult, setDijkstraStart, setScheduleArray, setTrainGenerator
 };
